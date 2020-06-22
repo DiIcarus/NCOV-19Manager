c4b993e5d8c4923735ebce01bd4259560f15d28a
sonnlh.deephub <sonnlh@deephub.vn>
2020-06-22 21:58:21 +0700

    Push

---
+++ client/build/precache-manifest.79bd92d0c8be028edbba2743f0590f49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d6a4ebee9632441a62c7144ed71a71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0aca311a1852a61f6424",
+    "url": "/static/css/main.361a89aa.chunk.css"
+  },
+  {
+    "revision": "3098c7ae8291ba8a9de8",
+    "url": "/static/js/2.bb2c5eff.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.bb2c5eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aca311a1852a61f6424",
+    "url": "/static/js/main.8d85605b.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
